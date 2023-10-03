a739c66d383adb9a40cb31b364f66671a1ad043b
0388824 <145066854+0388824@users.noreply.github.com>
2023-10-03 06:52:24 -0700

    week 3 homework

---
+++ week3/homework/main.js
@@ -0,0 +1,14 @@
+let changeImage1 = function() {
+    let fourthImage = document.getElementById("fourthImage");
+    fourthImage.src = "image1.png";
+}
+
+let changeImage2 = function() {
+    let fourthImage = document.getElementById("fourthImage");
+    fourthImage.src = "image2.png";
+}
+
+let changeImage3 = function() {
+    let fourthImage = document.getElementById("fourthImage");
+    fourthImage.src = "image3.png";
+}
